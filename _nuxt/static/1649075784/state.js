da942e807a39a9130e9904922a976094679d3a00
cabioch <cabioch@users.noreply.github.com>
2022-05-05 07:53:29 +0000

    deploy: cabioch/portfolio@4c81324ab9c09d6fad234f89b33ba458bf6c4774

--- _nuxt/static/1649075784/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649075784",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6ea1c73a"}}}}(null,"\u002F"));
